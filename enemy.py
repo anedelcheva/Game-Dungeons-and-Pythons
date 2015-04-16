d1924ad0beb4846fddfa61fc850a95fecff10052
anedelcheva <aneta.nedelcheva@gmail.com>
2015-04-16 19:46:26 +0300

    Adding attack() and test_attack()

--- enemy.py
+++ enemy.py
@@ -1,3 +1,7 @@
+from spell import Spell
+from weapon import Weapon
+
+
 class EnemyHasMaxHealth(Exception):
     pass
 
@@ -8,10 +12,12 @@ class CannotHealWithNonPositivePoints(Exception):
 
 class Enemy:
 
-    def __init__(self, health=100, mana=100, damage=20):
+    def __init__(self, health=100, mana=100, damage=20, weapon=None, spell=None):
         self.health = health
         self.mana = mana
         self.damage = damage
+        self.weapon = weapon
+        self.spell = spell
         self.max_health = 100
 
     def get_health(self):
@@ -19,6 +25,9 @@ class Enemy:
 
     def get_mana(self):
         return self.mana
+
+    def get_damage(self):
+        return self.damage
 
     def is_alive(self):
         return self.get_health() > 0
@@ -38,3 +47,29 @@ class Enemy:
         else:
             self.health += healing_points
         return True
+
+    def take_mana(self):
+        return False
+
+    def equip(self, weapon):
+        self.weapon = weapon
+
+    def learn(self, spell):
+        self.spell = spell
+
+    def attack(self, by):
+        if by == "weapon":
+            if self.weapon is None:
+                return 0
+            else:
+                return self.weapon.get_damage()
+        elif by == "spell" and self.can_cast():
+            if self.spell is None:
+                return 0
+            elif self.mana < self.spell.get_mana_cost():
+                raise Exception
+            else:
+                self.mana -= self.spell.get_mana_cost()
+                return self.spell.get_damage()
+        else:
+            self.get_damage()
--- enemy_test.py
+++ enemy_test.py
@@ -2,6 +2,8 @@ import unittest
 from enemy import Enemy
 from enemy import EnemyHasMaxHealth
 from enemy import CannotHealWithNonPositivePoints
+from weapon import Weapon
+from spell import Spell
 
 
 class TestEnemy(unittest.TestCase):
@@ -11,7 +13,9 @@ class TestEnemy(unittest.TestCase):
         self.enemy_1fourthalive = Enemy(25, 0, 20)
         self.enemy_dead = Enemy(0, 50, 20)
         self.enemy_halfalive = Enemy(50, 0, 20)
-        self.enemy_3fourthsalive = Enemy(75, 0, 20)
+        self.enemy_3fourthsalive = Enemy(75, 5, 60)
+        self.knife = Weapon("knife", 20)
+        self.spell = Spell("Fireball", 30, 50, 2)
 
     def test_init(self):
         self.assertTrue(isinstance(self.enemy_alive, Enemy))
@@ -45,6 +49,30 @@ class TestEnemy(unittest.TestCase):
         self.assertEqual(self.enemy_1fourthalive.get_health(), 100)
         self.enemy_3fourthsalive.take_healing(10)
         self.assertEqual(self.enemy_3fourthsalive.get_health(), 85)
+
+    def test_take_mana(self):
+        self.assertFalse(self.enemy_1fourthalive.take_mana())
+        self.assertFalse(self.enemy_3fourthsalive.take_mana())
+        self.assertFalse(self.enemy_alive.take_mana())
+        self.assertFalse(self.enemy_dead.take_mana())
+        self.assertFalse(self.enemy_halfalive.take_mana())
+
+    def test_equip(self):
+        self.enemy_alive.equip(self.knife)
+        self.assertEqual(self.enemy_alive.weapon, self.knife)
+
+    def test_learn(self):
+        self.enemy_halfalive.learn(self.spell)
+        self.assertEqual(self.enemy_halfalive.spell, self.spell)
+
+    def test_attack(self):
+        self.assertEqual(self.enemy_halfalive.attack(by="weapon"), 0)
+        self.assertEqual(self.enemy_1fourthalive.attack(by="spell"), None)
+        self.enemy_alive.equip(self.knife)
+        self.assertEqual(self.enemy_alive.attack(by="weapon"), 20)
+        self.enemy_3fourthsalive.learn(self.spell)
+        with self.assertRaises(Exception):
+            self.enemy_3fourthsalive.attack(by="spell")
 
 
 if __name__ == '__main__':
